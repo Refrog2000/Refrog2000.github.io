7686c932fa860c6c1f11bf3e03e2b646850ffcf6
refrog2000 <webmaster@refrog2000.com>
2021-04-22 12:08:22 +0800

    Website redirector for refrog2000.github.io

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>	
+		<title>中转页面</title>
+		<style>
+		</style>
+	</head>
+	<body>
+		<script>
+			setTimeout(function (){
+				window.location.assign("https://refrog2000.com/");
+			}, 100);
+		</script>
+	</body>
+</html>
